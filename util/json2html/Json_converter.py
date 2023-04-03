f45c977c0d847d1b052b36513f75597541591434
samiamin932 <samiamin123r@gmail.com>
2023-04-03 16:04:31 -0500

    added table of contents

--- util/json2html/Json_converter.py
+++ util/json2html/Json_converter.py
@@ -8,14 +8,16 @@ structured = {}
 # length of total text elements
 x = len(pdf["elements"])
 section = pdf["elements"][0]["Text"]
+section = section[:-1]
 structured[section] = ""
 
 # looping through text elements
 for i in range(x-1):
     if "Text" in pdf["elements"][i]:
-        if pdf["elements"][i]["Path"].find("Document/H")!=-1:
+        if pdf["elements"][i]["Path"].find("Document/H") != -1:
             section = pdf["elements"][i]["Text"]
-        if pdf["elements"][i]["Path"].find("Document/P")!=-1:
+            section = section[:-1]
+        if pdf["elements"][i]["Path"].find("Document/P") != -1:
             structured[section] = structured.get(section, "") + pdf["elements"][i]["Text"]
 
 f.close()
@@ -25,14 +27,17 @@ html = '''<html>
 <head>
 <title>Output HTML File</title>
 </head> 
-<body>
-<h1>           
+<body>  
+<h1>Table of contents</h1>
 '''
 
-#html = html + pdf["title"] + "</h1>\n"
+for i in structured:
+    html = html + "<a href=\"#" + i + "\">" + i + "</a>\n <br>\n"
 
 for i in structured:
+    html = html + "<div id=\"" + i + "\">\n"
     html = html + "<h2>" + i + "</h2>\n  <p>" + structured[i] + "</p>\n"
+    html = html + "</div>\n"
 
 html = html + """</body>
 </html>"""
