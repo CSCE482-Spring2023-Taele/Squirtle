b7f564560f281cc46d10c1ed903a1d66d1dbfbef
RobertYam7 <robert8y@gmail.com>
2023-04-16 23:21:44 -0500

    added logs for citation removal metrics

--- util/json2html/Json_converter.py
+++ util/json2html/Json_converter.py
@@ -117,6 +117,7 @@ class jsontohtml:
                             citations.append(text[starting_index - 1: ending_index])
                             starting_index = -1
                             ending_index = -1
+        print('CITATION MODEL --- Number of (name year)-style citations found: ' + len(citations))
         citations = list(set(citations))
         for citation in citations:
             # normalized_citation = '<!--' + citation + '-->'
@@ -128,6 +129,7 @@ class jsontohtml:
                 ending_bracket = text[bracket_citation_index:].find(']')
                 if ending_bracket <= bracket_citation_index + 5:
                     bracket_citations.append(text[bracket_citation_index: bracket_citation_index + ending_bracket + 1])
+        print('CITATION MODEL --- Number of [#]-style citations found: ' + len(bracket_citations))
         bracket_citations = list(set(bracket_citations))
         if len(bracket_citations) != 0:
             for bracket_citation in bracket_citations:
@@ -139,6 +141,8 @@ class jsontohtml:
             for legal_citation_index in legal_citation_indexes:
                 citation_span = legal_citation_index.full_span()
                 legal_citations.append(text[citation_span[0] : citation_span[1]])
+        print('CITATION MODEL --- Number of legal-style citations found: ' + len(legal_citations))
+        legal_citations = list(set(legal_citations))
         if len(legal_citations) != 0:
             for legal_citation in legal_citations:
                 text = text.replace(legal_citation, '')
